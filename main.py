bcba50a08e723ad47694fb96816b24714ef0d4c8
roni25536 <72336831+roni25536@users.noreply.github.com>
2022-11-01 15:27:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
-import logging
-import logging.handlers
-import os
-
+import telegram
 import requests
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
+def job():
+    TELEGRAM_BOT_TOKEN = '775005668:AAFLsiwi4ul8aBO3swzMkDmpkJmjjlT_3EA'
+    TELEGRAM_CHAT_ID = '@virtual_shipping'
+    webUrl = 'https://www.zipy.co.il/p/%D7%90%D7%9C%D7%99%D7%90%D7%A7%D7%A1%D7%A4%D7%A8%D7%A1/'
+    userName = "/#roni25536"
+    limit = 1
+    res = requests.get('https://www.zipy.co.il/api/product/getPromoProducts',
+                       params={'shopIds': ["aliexpress"], 'limit': limit})
+    res = res.json().get('result')
+    products = list(map(lambda p: {**p, 'url': webUrl + ('-'.join(p.get('name').lower().split(' '))) + '/' + p.get(
+        'id') + userName}, res))
+    messages = list(map(lambda p: {'image': p.get('image'),
+                                   'msg': "\n\n\nשם:    " +
+                                          p.get('name') +
+                                          "\n\nמחיר:    " +
+                                          str(p.get('price').get('value')) + p.get('price').get('icon') +
+                                          "\n\nבמקום:   " +
+                                          str(p.get('notDiscountedPrice').get('value')) + p.get(
+                                       'notDiscountedPrice').get('icon') +
+                                          "\n\nהנחה:   " +
+                                          str(p.get('discount')) + "%" +
+                                          "\n\nדירוג:    " +
+                                          str(p.get('reviewsAverage')) +
+                                          "\n\nתגובות:    " +
+                                          str(p.get('reviewsTotal')) +
+                                          "\n\nקישור:    " +
+                                          p.get('url')}, products))
+    bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
+    for p in messages:
+        bot.send_photo(chat_id=TELEGRAM_CHAT_ID, photo=p.get('image'), caption=p.get('msg'))
+    print('done')
 
-try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
-except KeyError:
-    SOME_SECRET = "Token not available!"
-    #logger.info("Token not available!")
-    #raise
-
-
-if __name__ == "__main__":
-    logger.info(f"Token value: {SOME_SECRET}")
-
-    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
-    if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Berlin: {temperature}')
+job()
